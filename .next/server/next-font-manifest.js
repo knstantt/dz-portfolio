40d2c0a24bb26ed3f5243ed167a522553ca28647
Konstantin Repyev <kostya.repyev@gmail.com>
2026-09-19 17:41:32 +0300

    fix: ignore .next and build artifacts properly

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{"/Users/konstantin/Desktop/claude1/dz-portfolio/app/layout":["static/media/48e2044251ef3125-s.p.woff2","static/media/904be59b21bd51cb-s.p.woff2"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
